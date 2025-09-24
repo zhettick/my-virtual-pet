9d5c44ab41e5a3dea8c320cad58175681d661fd9
zhettick <belikeotall@gmail.com>
2025-09-24 17:10:53 +0500

    kitty kitty

--- script1.js
+++ script1.js
@@ -23,25 +23,3 @@ function resetKitty() {
   kitty.src = "";               
   document.getElementById("status").innerText = "Your kitty is waiting...";
 }
-
-const button = document.getElementById("show-instructions");
-const instructions = document.getElementById("instructions");
-const cat = document.getElementById("cat");
-const scoreDisplay = document.getElementById("score");
-var score = 0;
-
-button.addEventListener("click", () => {
-  const isHidden = instructions.style.display === "none";
-  instructions.style.display = isHidden ? "block" : "none";
-  button.textContent = isHidden ? "Hide Instructions" : "Show Instructions";
-});
-
-cat.addEventListener("click", () => {
-  score++;
-  scoreDisplay.textContent = `Score: ${score}`;
-});
-
-setInterval(() => {
-  cat.style.left = Math.random() * 400 + "px";
-  cat.style.top = Math.random() * 300 + "px";
-}, 1500);
